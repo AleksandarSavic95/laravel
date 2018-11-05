c84c4e576c4b2d4e33ec69ecf172d3077fc2eafd
AleksandarSavic95 <alek_bn@hotmail.com>
2018-11-05 14:03:18 +0100

    basic auth made with artisan make:auth

--- database/migrations/2018_11_05_100357_create_users_table.php
+++ database/migrations/2018_11_05_100357_create_users_table.php
@@ -17,10 +17,12 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('password');
             $table->string('company');
             $table->string('country');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
